d925e0d9431988afbc24f5b1d6b1d099767ef423
mempo-dev2 <opensource@tigusoft.org>
2013-12-23 15:35:49 +0100

    [good] onverting windows-1250 to utf8 unicode with ICU library - working example.

--- icu_windows1250/x.cpp
+++ icu_windows1250/x.cpp
@@ -9,7 +9,7 @@ using std::string;
 using std::endl;
 using std::cout;
 
-int convert_windows1250_to_utf8() {
+std::string convert_windows1250_to_utf8(const std::string &bad) {
 	UConverter * converter;
 	UErrorCode err = U_ZERO_ERROR;
 	converter = ucnv_open( "windows-1250", &err );
@@ -21,33 +21,33 @@ int convert_windows1250_to_utf8() {
 		int32_t destCapacity = bad.size()+1;  // +1 just to be sure
 		UChar* dest = new UChar[destCapacity];
 		len = ucnv_toUChars(converter, dest,	destCapacity,   bad.c_str(),  bad.size(),  &err);
-		if (!U_SUCCESS(err2)) throw std::runtime_error("Can not convert.");
+		if (!U_SUCCESS(err)) throw std::runtime_error("Can not convert.");
 		// for (int32_t i=0; i<len; ++i) cout << (int)(unsigned char)dest[i] << ", ";  		cout<<endl;
 
-		UErrorCode err2 = U_ZERO_ERROR;
 		UConverter * converter2;
 		converter2 = ucnv_open("UTF-8", &err );
 		size_t len2=0;
-		if ( U_SUCCESS(err2) ) {
+		if ( U_SUCCESS(err) ) {
 			int32_t dest2Capacity =  (len+1) * 8*2;			// for worst case, 8 octet encoded, surrogate chars? TODO optimize
 			std::string dest2( dest2Capacity , 'X' ); 
-			cout << "String is:" << dest2 << endl;
 			char* dest2_as_sz = & dest2.at(0);
-			len2 = ucnv_fromUChars(converter2, dest2_as_sz,  dest2Capacity,   dest,  -1,  &err2);	
-			if (!U_SUCCESS(err2)) throw std::runtime_error("Can not convert 2.");
+			len2 = ucnv_fromUChars(converter2, dest2_as_sz,  dest2Capacity,   dest,  -1,  &err);	
+			if (!U_SUCCESS(err)) throw std::runtime_error("Can not convert 2.");
 			ucnv_close( converter2 );
+
+			return dest2.substr(0,len2); // *
+
 				// for (int32_t i=0; i<len2; ++i) cout << (int)(unsigned char)dest2[i] << ", ";  cout<<endl;
 		} else throw std::runtime_error("Can not open conv2.");
 	} else throw std::runtime_error("Can not open conv.");
 
-	cout << endl;
-
+	return "error_unicode";
 }
 
 
 int main() {
-	string bad = "Pawe\xB3. a\xC6\xE6"; // aĆć
-	cout << convert_windows1250_to_utf8( bad );
+	string bad = "Pawe\xB3. a\xC6\xE6"; // ł , Ćć
+	cout << convert_windows1250_to_utf8( bad ) << endl;
 }
 
 
