57081d25fa16cb6d3eaa0b9628df3c48a3a64d7c
lightpaint@tigusoft.pl <lightpaint@tigusoft.pl>
2015-03-19 21:39:16 +0100

    typo doc

--- scripts/anchor_sshcli/install.sh
+++ scripts/anchor_sshcli/install.sh
@@ -15,7 +15,7 @@ echo "Ok, created symlink in $installdir"
 
 echo "now will open editor for srv1 (vim)" ; read  _
 vim conf-srv1.local.sh
-echo "now will open editor for srv1 (vim)" ; read  _
+echo "now will open editor for srv2 (vim)" ; read  _
 vim conf-srv2.local.sh
 
 echo "REMEMBER to follow other instructions from install.txt e.g. configure crontab, and so on!"
